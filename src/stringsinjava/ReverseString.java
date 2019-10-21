51f00e05d777ad47f9cbabbe0b113881cb5c9858
Rakesh ch <rakesh.lkrnar@gmail.com>
2019-10-21 16:58:51 +0530

    Added strigs package

---
+++ src/stringsinjava/ReverseString.java
@@ -0,0 +1,18 @@
+package stringsinjava;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String name="Rakesh";
+		String revName="";
+		
+		for(int i=name.length()-1; i>=0; i--)
+		{
+		revName=revName+name.charAt(i);	
+		}
+		
+		System.out.println(revName);
+
+	}
+
+}
